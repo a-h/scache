65a0b8a471db7832af48b32155751600469de79b
Adrian Hesketh <adrianhesketh@hushmail.com>
2018-06-11 12:07:53 +0100

    Improved kinesis handling.

--- expiry/stream.go
+++ expiry/stream.go
@@ -5,11 +5,24 @@ import (
 	"encoding/json"
 	"fmt"
 	"math/rand"
+	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/kinesis"
 )
+
+type StreamData struct {
+	Keys []string
+	Time time.Time
+}
+
+func NewStreamData(keys []string) StreamData {
+	return StreamData{
+		Keys: keys,
+		Time: time.Now().UTC(),
+	}
+}
 
 // Pusher provides a way to push events to Kinesis.
 type Pusher struct {
@@ -42,7 +55,8 @@ func getRecords(keys []string) ([]*kinesis.PutRecordsRequestEntry, error) {
 	records := make([]*kinesis.PutRecordsRequestEntry, len(keys))
 	// Chunk into 512KB messages, Kinesis's maximum is 1MB.
 	for i, sliceOfKeys := range chunk(keys, 512*1024) {
-		data, err := json.Marshal(sliceOfKeys)
+		sc := NewStreamData(sliceOfKeys)
+		data, err := json.Marshal(sc)
 		if err != nil {
 			return records, err
 		}
@@ -82,7 +96,7 @@ type ShardID string
 type SequenceNumber string
 type StreamPosition map[ShardID]SequenceNumber
 
-func (p Pusher) Get(from StreamPosition) (keys []string, to StreamPosition, err error) {
+func (p Pusher) Get(from StreamPosition, since time.Time) (keys []string, to StreamPosition, err error) {
 	shards, err := p.ListAllShards()
 	if err != nil {
 		err = fmt.Errorf("Get: failed to list all shards: %v", err)
@@ -91,7 +105,7 @@ func (p Pusher) Get(from StreamPosition) (keys []string, to StreamPosition, err 
 	for _, shardID := range shards {
 		//TODO: Check that the sequence number date makes sense. If we haven't got anything in our cache, we don't care.
 		//TODO: We only care about changes made to data equal to or after the oldest date data currently in the cache.
-		shardKeys, t, read, getRecordsError := p.GetAllRecords(shardID, from[shardID])
+		records, t, read, getRecordsError := p.GetAllRecords(shardID, from[shardID])
 		if getRecordsError != nil {
 			err = fmt.Errorf("Get: failed to get records: %v", getRecordsError)
 			return
@@ -99,13 +113,20 @@ func (p Pusher) Get(from StreamPosition) (keys []string, to StreamPosition, err 
 		if !read {
 			continue
 		}
-		keys = append(keys, shardKeys...)
+		data, getDataError := getDataFromRecords(records)
+		if getDataError != nil {
+			err = fmt.Errorf("Get: failed to get data from records: %v", getDataError)
+			return
+		}
+		for _, d := range dataSince(data, since) {
+			keys = append(keys, d.Keys...)
+		}
 		to[shardID] = t
 	}
 	return
 }
 
-func (p Pusher) GetAllRecords(shard ShardID, from SequenceNumber) (keys []string, to SequenceNumber, read bool, err error) {
+func (p Pusher) GetAllRecords(shard ShardID, from SequenceNumber) (records []*kinesis.Record, to SequenceNumber, read bool, err error) {
 	svc := kinesis.New(session.New())
 
 	gsii := &kinesis.GetShardIteratorInput{
@@ -134,16 +155,33 @@ func (p Pusher) GetAllRecords(shard ShardID, from SequenceNumber) (keys []string
 			return
 		}
 		for _, r := range gro.Records {
-			var keySlice []string
-			err = json.Unmarshal(r.Data, &keySlice)
-			if err != nil {
-				err = fmt.Errorf("Get: failed to get unmarshal data for record '%v' for shard '%v' with shard iterator '%v': %v", r.SequenceNumber, shardID, itr.ShardIterator, err)
-				return
-			}
-			keys = append(keys, keySlice...)
+			records = append(records, r)
 			to = SequenceNumber(*r.SequenceNumber)
+			read = true
 		}
 		si = gro.NextShardIterator
+	}
+	return
+}
+
+func getDataFromRecords(records []*kinesis.Record) (data []StreamData, err error) {
+	for _, r := range records {
+		var sd StreamData
+		err = json.Unmarshal(r.Data, &sd)
+		if err != nil {
+			err = fmt.Errorf("Get: failed to get unmarshal data for record '%v' for shard '%v' with shard iterator '%v': %v", r.SequenceNumber, shardID, itr.ShardIterator, err)
+			return
+		}
+		data = append(data, sd)
+	}
+	return
+}
+
+func dataSince(in []StreamData, t time.Time) (out []StreamData) {
+	for _, d := range in {
+		if d.Time.Equal(t) || d.Time.After(t) {
+			out = append(out, d)
+		}
 	}
 	return
 }
