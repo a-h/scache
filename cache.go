130f102328e919ef2facd31003cf939a675f0fcc
Adrian Hesketh <adrianhesketh@hushmail.com>
2018-06-12 10:39:31 +0100

    Added tests.

--- cache.go
+++ cache.go
@@ -1,16 +1,23 @@
 package scache
 
 import (
+	"math/rand"
 	"sync"
 	"time"
 )
 
 // A DataID is a string which uniquely identifies a piece of data.
-type DataID string
+type DataID struct {
+	Source string
+	ID     string
+}
 
 // NewDataID creates a new DataID, e.g. source = "mydb.mytable.id", id=12345
 func NewDataID(source, id string) DataID {
-	return DataID(source + "=" + id)
+	return DataID{
+		Source: source,
+		ID:     id,
+	}
 }
 
 // NewCacheItem creates a new cache item.
@@ -29,26 +36,50 @@ type CacheItem struct {
 	Expiry time.Time
 }
 
+// DefaultExpiration is the default expiration function, it caches for at least one hour,
+// randomising expiry within a further 30 minute range.
+var DefaultExpiration = func(now func() time.Time) time.Time {
+	return now().Add(time.Hour * 1).Add(time.Duration(rand.Intn(30)) * time.Minute)
+}
+
 // New creates a new Cache.
 func New() *Cache {
 	return &Cache{
-		Now: time.Now,
+		Now:        time.Now,
+		Expiration: DefaultExpiration,
 	}
 }
 
 // Cache is a concurrent cache for storing data.
 type Cache struct {
-	Data sync.Map
-	Now  func() time.Time
+	Data       sync.Map
+	Now        func() time.Time
+	Expiration func(now func() time.Time) time.Time
 }
 
 // Put some data into the cache.
-func (c *Cache) Put(id DataID, item interface{}, expireAfter time.Duration) {
-	c.Data.Store(id, NewCacheItem(item, c.Now().Add(expireAfter)))
+func (c *Cache) Put(id DataID, item interface{}) {
+	expiryTime := c.Expiration(c.Now)
+	c.PutCacheItem(id, NewCacheItem(item, expiryTime))
+}
+
+// PutCacheItem puts a cache item into memory.
+func (c *Cache) PutCacheItem(id DataID, item CacheItem) {
+	c.Data.Store(id, item)
 }
 
 // Get some data from the cache.
-func (c *Cache) Get(id DataID) (item CacheItem, ok bool) {
+func (c *Cache) Get(id DataID) (value interface{}, ok bool) {
+	var ci CacheItem
+	ci, ok = c.GetItem(id)
+	if ok {
+		value = ci.Item
+	}
+	return
+}
+
+// GetItem gets the item from the cache.
+func (c *Cache) GetItem(id DataID) (item CacheItem, ok bool) {
 	d, ok := c.Data.Load(id)
 	if !ok {
 		return
@@ -66,7 +97,7 @@ func (c *Cache) Remove(id DataID) {
 func (c *Cache) RemoveExpired() {
 	remover := func(k, v interface{}) bool {
 		item := v.(CacheItem)
-		if item.Expiry.After(c.Now()) {
+		if item.Expiry.Before(c.Now()) {
 			c.Data.Delete(k)
 		}
 		return true
@@ -75,7 +106,7 @@ func (c *Cache) RemoveExpired() {
 }
 
 // RemoveByIDs removes values from the cache by their ID.
-func (c *Cache) RemoveByIDs(ids []DataID) {
+func (c *Cache) RemoveByIDs(ids ...DataID) {
 	for _, id := range ids {
 		c.Remove(id)
 	}
---
+++ cache_test.go
@@ -0,0 +1,72 @@
+package scache
+
+import (
+	"testing"
+	"time"
+)
+
+func TestCachePutAndGet(t *testing.T) {
+	c := New()
+	input := "the thing to cache"
+	c.Put(NewDataID("mydb.table.tableid", "12345"), input)
+	output, ok := c.Get(NewDataID("mydb.table.tableid", "12345"))
+	if !ok {
+		t.Fatal("could not get data we just put in")
+	}
+	if output != input {
+		t.Errorf("expected input and output to be equal, got '%v'", output)
+	}
+}
+
+func TestCacheItemsCanExpire(t *testing.T) {
+	c := New()
+	expiry := time.Now().Add(time.Second * -1)
+	c.PutCacheItem(NewDataID("mydb.table.tableid", "12345"), NewCacheItem("item", expiry))
+	_, ok := c.Get(NewDataID("mydb.table.tableid", "12345"))
+	if !ok {
+		t.Fatal("haven't removed expired items yet")
+	}
+	c.RemoveExpired()
+	_, ok = c.Get(NewDataID("mydb.table.tableid", "12345"))
+	if ok {
+		t.Fatal("expired items should have been removed")
+	}
+}
+
+func TestCacheItemsCanBeOverwritten(t *testing.T) {
+	id := NewDataID("mydb.table.tableid", "1")
+	c := New()
+	c.Put(id, "v1")
+	c.Put(id, "v2")
+	ci, ok := c.Get(id)
+	if !ok {
+		t.Error("expected to be able to get the value back")
+	}
+	if ci.(string) != "v2" {
+		t.Errorf("expected 'v2', got '%v'", ci)
+	}
+}
+
+func TestCacheItemsCanBeRemoved(t *testing.T) {
+	c := New()
+	id1, id2, id3 := NewDataID("mydb.table.tableid", "1"),
+		NewDataID("mydb.table.tableid", "2"),
+		NewDataID("mydb.table.tableid", "3")
+	c.Put(id1, "item")
+	c.Put(id2, "item")
+	c.Put(id3, "item")
+	c.RemoveByIDs(id1, id2)
+	if _, ok := c.Get(id1); ok {
+		t.Errorf("expected %v to have been removed, but it wasn't", id1)
+	}
+	if _, ok := c.Get(id2); ok {
+		t.Errorf("expected %v to have been removed, but it wasn't", id2)
+	}
+	if _, ok := c.Get(id3); !ok {
+		t.Errorf("expected %v to still be accessible, but it wasn't", id3)
+	}
+	c.Remove(id3)
+	if _, ok := c.Get(id3); ok {
+		t.Errorf("expected %v to have been removed, but it wasn't", id3)
+	}
+}
