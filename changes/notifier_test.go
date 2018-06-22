d07b609638e85cf65b864fe1aaa93349902b6755
Adrian Hesketh <adrianhesketh@hushmail.com>
2018-06-22 08:50:47 +0100

    Completed notifier tests.

--- changes/notifier_test.go
+++ changes/notifier_test.go
@@ -31,14 +31,23 @@ func (u UserRecord) ObservableID() data.ID {
 func TestNotifier(t *testing.T) {
 	id1 := data.NewID("db.users.userid", "1")
 	id2 := data.NewID("db.users.userid", "2")
+	id3 := data.NewID("db.users.userid", "3")
+	id4 := data.NewID("db.users.userid", "4")
 
 	expected1 := []string{id1.String(), id2.String()}
+	expected2 := []string{id3.String(), id4.String()}
 
 	putter := &MockStreamPutter{
 		PutFuncs: []func(keys []string) error{
 			func(keys []string) (err error) {
 				if !reflect.DeepEqual(keys, expected1) {
 					t.Errorf("put #1: expected %v, got %v", expected1, keys)
+				}
+				return
+			},
+			func(keys []string) (err error) {
+				if !reflect.DeepEqual(keys, expected2) {
+					t.Errorf("put #2: expected %v, got %v", expected2, keys)
 				}
 				return
 			},
@@ -62,6 +71,10 @@ func TestNotifier(t *testing.T) {
 
 	err := n.NotifyObservablesChanged(records...)
 	if err != nil {
-		t.Errorf("unexpected error on notification: %v", err)
+		t.Errorf("unexpected error on NotifyObservablesChanged: %v", err)
+	}
+	err = n.NotifyDataChanged(id3, id4)
+	if err != nil {
+		t.Errorf("unexpected error on NotifyDataChanged: %v", err)
 	}
 }
